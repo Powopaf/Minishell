113504442d06a8917df74cedaf5d0314027d4fe9
Pierre-Antoine Fournier <pifourni@f6r4s6.clusters.42paris.fr>
2026-02-13 09:25:24 +0100

    add export

--- src/func/func.h
+++ src/func/func.h
@@ -6,7 +6,7 @@
 /*   By: pifourni <pifourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 15:21:54 by pifourni          #+#    #+#             */
-/*   Updated: 2026/02/12 19:43:53 by pifourni         ###   ########.fr       */
+/*   Updated: 2026/02/13 09:24:49 by pifourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	echo(char **args, t_shell *sh);
 void	pwd(char **args, t_shell *sh);
 void	cd(t_shell *sh, char **args);
 void	env(char **args, t_shell *sh);
+void	export(char **args, t_shell *sh);
 
 #endif
