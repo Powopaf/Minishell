f9c5dcc2e89730ad4427cfb04ea02b51e2ffd125
R-FLM <79423877+R-FLM@users.noreply.github.com>
2026-02-25 12:19:14 +0100

    Pwd fixed (#63)
    * fixed pwd using getcwd
    
    * removed path_max
    
    ---------
    
    Co-authored-by: flomulle <flomulle@student.42.fr>

--- src/func/pwd.c
+++ src/func/pwd.c
@@ -3,44 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pifourni <pifourni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flomulle <flomulle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 13:18:42 by pifourni          #+#    #+#             */
-/*   Updated: 2026/02/23 13:00:59 by pifourni         ###   ########.fr       */
+/*   Updated: 2026/02/25 12:17:40 by flomulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../libft/libft.h"
 #include "../error/err.h"
 #include "../exec/parser_cmd/parser_cmd.h"
 #include "func.h"
-#include "../../libft/libft.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void	pwd(char **args, t_shell *sh)
 {
-	char	*pwd;
-	
+	char	*cwd;
+
 	(void)args;
-	pwd = get_env(sh, "PWD");
-	if (!pwd || pwd[0] == '\0')
+	cwd = NULL;
+	cwd = getcwd(NULL, 0);
+	if (cwd)
 	{
-		free(pwd);
-		pwd = get_env(sh, "OLDPWD");
-		if (!pwd || pwd[0] == '\0')
-		{
-			free(pwd);
-			sh->status = EXIT_FAILURE;
-			return ;
-		}
-	}
-	if (printf("%s\n", pwd) < 0)
-	{
-		error(sh, "pwd", "write error", 1);
-		free(pwd);
+		cwd = ft_strjoin_char_p(&cwd, '\n', 1, 1);
+		write(1, cwd, ft_strlen(cwd));
+		if (cwd)
+			free(cwd);
+		sh->status = 0;
 		return ;
 	}
-	free(pwd);
-	sh->status = EXIT_SUCCESS;
+	error(sh, "pwd", strerror(errno), -FAIL);
 	return ;
 }
