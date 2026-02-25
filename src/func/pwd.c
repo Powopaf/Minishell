ee83cbfb304d8ab19dbf50d296f91ed1d17e815b
flomulle <flomulle@student.42.fr>
2026-02-25 12:18:05 +0100

    removed path_max

--- src/func/pwd.c
+++ src/func/pwd.c
@@ -6,52 +6,20 @@
 /*   By: flomulle <flomulle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 13:18:42 by pifourni          #+#    #+#             */
-/*   Updated: 2026/02/25 12:14:58 by flomulle         ###   ########.fr       */
+/*   Updated: 2026/02/25 12:17:40 by flomulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../libft/libft.h"
 #include "../error/err.h"
 #include "../exec/parser_cmd/parser_cmd.h"
 #include "func.h"
-#include "../../libft/libft.h"
-#include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
-#ifndef PATH_MAX
-# define PATH_MAX 4096
-#endif
-
-// void	pwd(char **args, t_shell *sh)
-// {
-// 	char	*pwd;
-
-// 	(void)args;
-// 	pwd = get_env(sh, "PWD");
-// 	if (!pwd || pwd[0] == '\0')
-// 	{
-// 		free(pwd);
-// 		pwd = get_env(sh, "OLDPWD");
-// 		if (!pwd || pwd[0] == '\0')
-// 		{
-// 			free(pwd);
-// 			sh->status = EXIT_FAILURE;
-// 			return ;
-// 		}
-// 	}
-// 	if (printf("%s\n", pwd) < 0)
-// 	{
-// 		error(sh, "pwd", "write error", 1);
-// 		free(pwd);
-// 		return ;
-// 	}
-// 	free(pwd);
-// 	sh->status = EXIT_SUCCESS;
-// 	return ;
-// }
+#include <unistd.h>
 
 void	pwd(char **args, t_shell *sh)
 {
-	char *cwd;
+	char	*cwd;
 
 	(void)args;
 	cwd = NULL;
